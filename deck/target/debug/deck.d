1200af9bbc2797556c73671a5e38ede4095e3efd
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2026-06-15 11:48:32 -0300

    first Commit

---
+++ deck/target/debug/deck.d
@@ -0,0 +1,1 @@
+/var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/target/debug/deck: /var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/src/main.rs
---
+++ deck/target/debug/deps/deck-d62ef219b7992316.d
@@ -0,0 +1,5 @@
+/var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/target/debug/deps/deck-d62ef219b7992316.d: src/main.rs
+
+/var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/target/debug/deps/deck-d62ef219b7992316: src/main.rs
+
+src/main.rs:
