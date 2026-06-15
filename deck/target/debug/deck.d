5b0acba52bce98959521a536d7414dd76cf97cb4
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2026-06-15 11:51:41 -0300

    remove target folder.

--- deck/target/debug/deck.d
+++
@@ -1,1 +0,0 @@
-/var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/target/debug/deck: /var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/src/main.rs
--- deck/target/debug/deps/deck-d62ef219b7992316.d
+++
@@ -1,5 +0,0 @@
-/var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/target/debug/deps/deck-d62ef219b7992316.d: src/main.rs
-
-/var/home/rodrigo/Projects/Courses/Rust/rust_repo/deck/target/debug/deps/deck-d62ef219b7992316: src/main.rs
-
-src/main.rs:
